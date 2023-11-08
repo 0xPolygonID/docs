b87c9077728648b1a21f6ad855d45926483c65fe
Alex Monso <amonsosanz@gmail.com>
2023-11-08 18:01:54 +0100

    Revert formatting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
-const math = require("remark-math");
-const katex = require("rehype-katex");
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 module.exports = {
   title: "Polygon ID Documentation",
@@ -14,63 +14,58 @@ module.exports = {
     description: "Build your next blockchain dApp using Polygon ID.",
   },
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-    path: "i18n",
+    defaultLocale: 'en',
+    locales: ['en'],
+    path: 'i18n',
     localeConfigs: {
       en: {
-        label: "English",
-        direction: "ltr",
-        htmlLang: "en-US",
-        calendar: "gregory",
-        path: "en",
+        label: 'English',
+        direction: 'ltr',
+        htmlLang: 'en-US',
+        calendar: 'gregory',
+        path: 'en',
       },
     },
   },
 
-  onBrokenLinks: "log",
+  onBrokenLinks: 'log',
   themeConfig: {
     announcementBar: {
-      id: "learn_more",
+      id: 'learn_more',
       content:
-        'New release: On-chain issuer, Schema repository & builder <a class="announcement-link" target="_blank" rel="noopener noreferrer" href="https://polygon.technology/blog/polygon-id-release-4-new-tools-seamless-integration"> Learn more</a>',
-      backgroundColor: "#20232a",
-      textColor: "#fff",
+          'New release: On-chain issuer, Schema repository & builder <a class="announcement-link" target="_blank" rel="noopener noreferrer" href="https://polygon.technology/blog/polygon-id-release-4-new-tools-seamless-integration"> Learn more</a>',
+      backgroundColor: '#20232a',
+      textColor: '#fff',
       isCloseable: false,
     },
-    metadata: [
-      {
-        name: "description",
-        content:
-          "Welcome to Polygon ID Documentation, the official documentation for Polygon ID.",
-      },
-    ],
+    metadata: [{name: 'description', content: 'Welcome to Polygon ID Documentation, the official documentation for Polygon ID.'}],
     colorMode: {
-      defaultMode: "dark",
+      defaultMode: 'dark',
       disableSwitch: true,
       respectPrefersColorScheme: false,
     },
 
     footer: {
-      style: "dark",
-      links: [],
+      style: 'dark',
+      links: [
+      ],
     },
 
-    image: "img/thumbnail.jpg",
+    image: 'img/thumbnail.jpg',
     prism: {
       theme: require("prism-react-renderer/themes/github"),
       darkTheme: require("prism-react-renderer/themes/dracula"),
       defaultLanguage: "javascript",
-      additionalLanguages: ["solidity"],
+      additionalLanguages: ['solidity']
     },
     algolia: {
       // need to update this
       indexName: "devs-polygonid",
-      appId: "VHJ21ILMOQ",
+      appId: 'VHJ21ILMOQ',
       apiKey: "2281aa8fc382facc33ebfbb1e745e442",
       contextualSearch: true,
       algoliaOptions: {
-        attributesToSnippet: ["content:20"],
+        attributesToSnippet: ['content:20'],
       },
     },
     navbar: {
@@ -79,115 +74,116 @@ module.exports = {
         alt: "Polygon logo",
         src: "img/polygon-id.svg",
         srcDark: "img/polygon-id.svg",
-        href: "/",
+        href: '/',
         target: "_self",
-      },
+       },
       items: [
         {
-          position: "left",
-          label: "Build",
+          position: 'left',
+          label: 'Build',
           items: [
             {
-              href: "/docs/issuer/issuer-node-ui",
-              label: "Issuer API+UI",
+              href: '/docs/issuer/issuer-node-ui',
+              label: 'Issuer API+UI',
               rel: null,
             },
             {
-              href: "/docs/verifier/verifier-overview/",
-              label: "Verifier SDK",
+              href: '/docs/verifier/verifier-overview/',
+              label: 'Verifier SDK',
               rel: null,
             },
             {
-              href: "/docs/wallet/wallet-overview/",
-              label: "Wallet SDK",
+              href: '/docs/wallet/wallet-overview/',
+              label: 'Wallet SDK',
               rel: null,
             },
             {
-              href: "/docs/js-sdk/js-sdk-overview/",
-              label: "JS-SDK",
+              href: '/docs/js-sdk/js-sdk-overview/',
+              label: 'JS-SDK',
               rel: null,
-            },
-          ],
+            }
+          ]
         },
         {
-          position: "left",
-          label: "Tools",
+          position: 'left',
+          label: 'Tools',
           items: [
             {
-              href: "https://schema-builder.polygonid.me/",
-              label: "Schema Explorer",
-              target: "_blank",
+              href: 'https://schema-builder.polygonid.me/',
+              label: 'Schema Explorer',
+              target: '_blank',
               rel: null,
             },
             {
-              href: "https://schema-builder.polygonid.me/builder",
-              label: "Schema Builder",
-              target: "_blank",
+              href: 'https://schema-builder.polygonid.me/builder',
+              label: 'Schema Builder',
+              target: '_blank',
               rel: null,
             },
             {
-              href: "https://schema-builder.polygonid.me/query-builder",
-              label: "Query Builder",
-              target: "_blank",
+              href: 'https://schema-builder.polygonid.me/query-builder',
+              label: 'Query Builder',
+              target: '_blank',
               rel: null,
             },
             {
-              href: "/fakeurl",
-              label: "JWZ (coming soon)",
-              target: "_blank",
+              href: '/fakeurl',
+              label: 'JWZ (coming soon)',
+              target: '_blank',
               className: "disabled",
             },
-          ],
+          ]
         },
         {
-          position: "left",
-          label: "Learn",
+          position: 'left',
+          label: 'Learn',
           items: [
             {
-              href: "https://www.youtube.com/playlist?list=PLRD3rkREa7mLLJ6jfUTygXV1iK_AvsDdw",
-              label: "Fundamentals",
-              target: "_blank",
+              href: 'https://www.youtube.com/playlist?list=PLRD3rkREa7mLLJ6jfUTygXV1iK_AvsDdw',
+              label: 'Fundamentals',
+              target: '_blank',
               rel: null,
             },
             {
-              href: "https://www.youtube.com/playlist?list=PLRD3rkREa7mLjDB1qL4KkCtob6kPJIvhS",
-              label: "Architecture",
-              target: "_blank",
+              href: 'https://www.youtube.com/playlist?list=PLRD3rkREa7mLjDB1qL4KkCtob6kPJIvhS',
+              label: 'Architecture',
+              target: '_blank',
               rel: null,
             },
             {
-              href: "https://www.youtube.com/playlist?list=PLRD3rkREa7mIgx_RHZdin74vEb2A1Evek",
-              label: "Inside Polygon ID",
-              target: "_blank",
+              href: 'https://www.youtube.com/playlist?list=PLRD3rkREa7mIgx_RHZdin74vEb2A1Evek',
+              label: 'Inside Polygon ID',
+              target: '_blank'
             },
-          ],
+          ]
+
         },
         {
-          position: "left",
-          label: "Releases",
-          href: "/docs/releases",
+          position: 'left',
+          label: 'Releases',
+          href: '/docs/releases'
         },
         {
-          position: "left",
-          label: "Support",
-          href: "https://support.polygon.technology/support/solutions/82000473421",
-          target: "_blank",
+          position: 'left',
+          label: 'Support',
+          href: 'https://support.polygon.technology/support/solutions/82000473421',
+          target: '_blank',
         },
         {
-          position: "left",
-          label: "Protocol",
+          position: 'left',
+          label: 'Protocol',
           items: [
             {
-              href: "https://docs.iden3.io/",
-              label: "Iden3 General Docs",
-              target: "_blank",
+              href: 'https://docs.iden3.io/',
+              label: 'Iden3 General Docs',
+              target: '_blank',
             },
             {
-              href: "https://iden3-communication.io/",
-              label: "Iden3comm",
-              target: "_blank",
+              href: 'https://iden3-communication.io/',
+              label: 'Iden3comm',
+              target: '_blank',
             },
-          ],
+          ]
         },
         {
           href: "https://github.com/0xPolygonID",
@@ -222,16 +218,14 @@ module.exports = {
       "@docusaurus/preset-classic",
       {
         docs: {
-          routeBasePath: "/docs",
+          routeBasePath: '/docs',
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl: "https://github.com/0xPolygonID/id-docs/tree/main",
           path: "docs",
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           remarkPlugins: [math],
-          rehypePlugins: [
-            [katex, { strict: false, throwOnError: true, globalGroup: true }],
-          ],
+          rehypePlugins: [[katex, {strict: false, throwOnError: true,globalGroup: true}]],
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
