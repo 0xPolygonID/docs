cc82e6feef64189d0d03a1384cfa33363b52f0d9
Vinícius Gomes <39063808+cerberushades@users.noreply.github.com>
2023-11-13 16:37:21 -0300

    adds more info about R%

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ module.exports = {
     announcementBar: {
       id: 'learn_more',
       content:
-          'New Release: Credentials Marketplace, On-chain Issuer and Issuer Node Improvements <a class="announcement-link" target="_blank" rel="noopener noreferrer" href="/docs/release-notes/r5"> Learn more</a>',
+          'New Release: Credentials Marketplace, On-chain Issuer and Issuer Node Improvements <a class="announcement-link" target="_blank" rel="noopener noreferrer" href="/docs/release-notes/r5"> Learn more</a>  <a class="announcement-link" target="_blank" rel="noopener noreferrer" href="https://polygon.technology/blog/polygon-id-release-5-boosting-credential-liquidity?utm_source=twitter&utm_medium=social&utm_content=id-r5-blog"> Check our blog</a>',
       backgroundColor: '#20232a',
       textColor: '#fff',
       isCloseable: false,
