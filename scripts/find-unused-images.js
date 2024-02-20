282fc94aed62040d1590218cd134ea30400a803d
Alex Monso <amonsosanz@gmail.com>
2024-02-20 13:01:15 +0100

    Include root path (non recursive) on find-unused-images.js

--- scripts/find-unused-images.js
+++ scripts/find-unused-images.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const rootDir = path.join(__dirname, '../');
 const staticDir = path.join(__dirname, '../static');
 const docsDir = path.join(__dirname, '../docs');
 const srcDir = path.join(__dirname, '../src');
+
+function readFiles(directory, fileList = []) {
+    const files = fs.readdirSync(directory);
+    files.forEach(file => {
+        const filePath = path.join(directory, file);
+        if (!fs.statSync(filePath).isDirectory()) {
+            fileList.push(filePath);
+        }
+    });
+    return fileList;
+}
 
 function readFilesRecursively(directory, fileList = []) {
     const files = fs.readdirSync(directory);
@@ -33,13 +45,14 @@ function removeFile(filePath) {
 }
 
 function findUnusedImages(remove = false) {
-    const allImages = readFilesRecursively(staticDir).filter(file => file.match(/\.(jpg|jpeg|png|gif|svg)$/));
+    const allImages = readFilesRecursively(staticDir).filter(file => file.match(/\.(jpg|jpeg|png|gif|svg|mp4)$/));
+    const root = readFiles(rootDir).filter(file => file.match(/\.(md|js)$/));
     const allDocs = readFilesRecursively(docsDir).filter(file => file.match(/\.(md|mdx)$/));
     const allSrc = readFilesRecursively(srcDir).filter(file => file.match(/\.(js|css|scss)$/));
 
     let unusedImages = [...allImages];
 
-    [...allDocs, ...allSrc].forEach(doc => {
+    [...root, ...allDocs, ...allSrc].forEach(doc => {
         const content = fs.readFileSync(doc, 'utf8');
         allImages.forEach(image => {
             if (isImageUsedInFile(image, content)) {
@@ -49,6 +62,7 @@ function findUnusedImages(remove = false) {
     });
 
     console.dir(unusedImages, {'maxArrayLength': null} );
+    console.log(unusedImages.length, 'unused images found');
 
     if (remove) {
         unusedImages.forEach(imagePath => {
