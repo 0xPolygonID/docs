9b83a8bfb1d04ca5c31d60bb2a847ee26c7bb643
Alex Monso <amonsosanz@gmail.com>
2024-02-20 17:38:44 +0100

    Imporve regex and format scripts

--- scripts/find-broken-image-links.js
+++ scripts/find-broken-image-links.js
@@ -1,37 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
 
-const docsPath = './docs';
-const staticImgPath = './static';
+const docsPath = "./docs";
+const staticImgPath = "./static";
 
 function checkImageExists(imagePath) {
-    const fullPath = path.join(staticImgPath, imagePath);
-    if (!fs.existsSync(fullPath)) {
-        console.log(`Image not found: ${fullPath}`);
-        return false;
-    }
-    return true;
+  const fullPath = path.join(staticImgPath, imagePath);
+  if (!fs.existsSync(fullPath)) {
+    console.log(`Image not found: ${fullPath}`);
+    return false;
+  }
+  return true;
 }
 
 function validateImageLinks() {
-    const files = glob.sync(`${docsPath}/**/*.+(md|mdx)`);
+  const files = glob.sync(`${docsPath}/**/*.+(md|mdx)`);
+  let allImagePaths = [];
+
+  files.forEach((file) => {
+    const content = fs.readFileSync(file, "utf8");
 
     const markdownImageRegex = /!\[.*?\]\((.*?)\)/g;
-    const htmlImageRegex = /<img.*?src=(?:\{useBaseUrl\(["'](.+?)["']\)\}|["'](.+?)["']).*?>/g;
+    const htmlImageRegex =
+      /<img.*?\s+src\s*=\s*(?:\{useBaseUrl\(["'](.+?)["']\)\}|["'](.+?)["']).*?>/g;
 
-    files.forEach(file => {
-        const content = fs.readFileSync(file, 'utf8');
-    
-        const matches = [...content.matchAll(markdownImageRegex), ...content.matchAll(htmlImageRegex)];
-        
-        matches.forEach(match => {
-            const imagePath = match[1] || match[2];
-            checkImageExists(imagePath.replace(/^\//, ''));
-        });
-    });
+    const markdownMatches = [...content.matchAll(markdownImageRegex)].map((match) => match[1]);
+    const htmlMatches = [...content.matchAll(htmlImageRegex)].map((match) => match[1] || match[2]);
 
-    console.log('Image link validation complete.');
+    allImagePaths = allImagePaths.concat(markdownMatches, htmlMatches);
+  });
+
+  // Deduplicate paths
+  const uniqueImagePaths = [...new Set(allImagePaths)];
+
+  uniqueImagePaths.forEach(checkImageExists);
+
+  console.log("Image link validation complete.");
 }
 
 validateImageLinks();
--- scripts/find-unused-images.js
+++ scripts/find-unused-images.js
@@ -1,74 +1,76 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const rootDir = path.join(__dirname, '../');
-const staticDir = path.join(__dirname, '../static');
-const docsDir = path.join(__dirname, '../docs');
-const srcDir = path.join(__dirname, '../src');
+const rootDir = path.join(__dirname, "../");
+const staticDir = path.join(__dirname, "../static");
+const docsDir = path.join(__dirname, "../docs");
+const srcDir = path.join(__dirname, "../src");
 
 function readFiles(directory, fileList = []) {
-    const files = fs.readdirSync(directory);
-    files.forEach(file => {
-        const filePath = path.join(directory, file);
-        if (!fs.statSync(filePath).isDirectory()) {
-            fileList.push(filePath);
-        }
-    });
-    return fileList;
+  const files = fs.readdirSync(directory);
+  files.forEach((file) => {
+    const filePath = path.join(directory, file);
+    if (!fs.statSync(filePath).isDirectory()) {
+      fileList.push(filePath);
+    }
+  });
+  return fileList;
 }
 
 function readFilesRecursively(directory, fileList = []) {
-    const files = fs.readdirSync(directory);
-    files.forEach(file => {
-        const filePath = path.join(directory, file);
-        if (fs.statSync(filePath).isDirectory()) {
-            readFilesRecursively(filePath, fileList);
-        } else {
-            fileList.push(filePath);
-        }
-    });
-    return fileList;
+  const files = fs.readdirSync(directory);
+  files.forEach((file) => {
+    const filePath = path.join(directory, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      readFilesRecursively(filePath, fileList);
+    } else {
+      fileList.push(filePath);
+    }
+  });
+  return fileList;
 }
 
 function isImageUsedInFile(imagePath, fileContent) {
-    return fileContent.includes(path.basename(imagePath));
+  return fileContent.includes(path.basename(imagePath));
 }
 
 function removeFile(filePath) {
-    fs.unlink(filePath, (err) => {
-        if (err) {
-            console.error(`Error removing file: ${filePath}`, err);
-            return;
-        }
-        console.log(`Removed: ${filePath}`);
-    });
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(`Error removing file: ${filePath}`, err);
+      return;
+    }
+    console.log(`Removed: ${filePath}`);
+  });
 }
 
 function findUnusedImages(remove = false) {
-    const allImages = readFilesRecursively(staticDir).filter(file => file.match(/\.(jpg|jpeg|png|gif|svg|mp4)$/));
-    const root = readFiles(rootDir).filter(file => file.match(/\.(md|js)$/));
-    const allDocs = readFilesRecursively(docsDir).filter(file => file.match(/\.(md|mdx)$/));
-    const allSrc = readFilesRecursively(srcDir).filter(file => file.match(/\.(js|css|scss)$/));
+  const allImages = readFilesRecursively(staticDir).filter((file) =>
+    file.match(/\.(jpg|jpeg|png|gif|svg|mp4)$/)
+  );
+  const root = readFiles(rootDir).filter((file) => file.match(/\.(md|js)$/));
+  const allDocs = readFilesRecursively(docsDir).filter((file) => file.match(/\.(md|mdx)$/));
+  const allSrc = readFilesRecursively(srcDir).filter((file) => file.match(/\.(js|css|scss)$/));
 
-    let unusedImages = [...allImages];
+  let unusedImages = [...allImages];
 
-    [...root, ...allDocs, ...allSrc].forEach(doc => {
-        const content = fs.readFileSync(doc, 'utf8');
-        allImages.forEach(image => {
-            if (isImageUsedInFile(image, content)) {
-                unusedImages = unusedImages.filter(unusedImage => unusedImage !== image);
-            }
-        });
+  [...root, ...allDocs, ...allSrc].forEach((doc) => {
+    const content = fs.readFileSync(doc, "utf8");
+    allImages.forEach((image) => {
+      if (isImageUsedInFile(image, content)) {
+        unusedImages = unusedImages.filter((unusedImage) => unusedImage !== image);
+      }
     });
+  });
 
-    console.dir(unusedImages, {'maxArrayLength': null} );
-    console.log(unusedImages.length, 'unused images found');
+  console.dir(unusedImages, { maxArrayLength: null });
+  console.log(unusedImages.length, "unused images found");
 
-    if (remove) {
-        unusedImages.forEach(imagePath => {
-            removeFile(imagePath);
-        });
-    }
+  if (remove) {
+    unusedImages.forEach((imagePath) => {
+      removeFile(imagePath);
+    });
+  }
 }
 
 findUnusedImages(false);
